c73724aa9d7261be2a5d9df844969df855b5ccb2
Darlan Mendonça <darlanmendonca@gmail.com>
2016-10-25 14:34:36 -0200

    add gzip to browser-sync middleware

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -2,10 +2,16 @@
 
 let gulp = require('gulp');
 let config = require('./gulp.config.js');
+let historyApi = require('connect-history-api-fallback');
+let gzip = require('compression');
 let options = config.browserSyncOptions;
 
 gulp.task('browser-sync', browserSyncTask);
 
 function browserSyncTask() {
+  options.middleware = [
+    historyApi(),
+    gzip(),
+  ],
   config.browserSync.init(options);
 }
